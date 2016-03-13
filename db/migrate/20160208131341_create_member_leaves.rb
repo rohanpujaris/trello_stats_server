15be0a573e860dc0100a1d8d0102da73adb0d664
Rohan <rohanpujaris@gmail.com>
2016-03-13 19:45:56 +0530

    Store trello card url. Correct migration name and deleted not required migration

--- db/migrate/20160208131341_create_member_leaves.rb
+++
@@ -1,8 +0,0 @@
-class CreateMemberLeaves < ActiveRecord::Migration
-  def change
-    create_table :member_leaves do |t|
-      t.references :member, index: true, foreign_key: true
-      t.date :date
-    end
-  end
-end
